117881a52a1b078d57bb42f0469af2074e984b79
Brett Mayson <brett@bmandesigns.com>
2017-09-17 19:53:00 -0600

    Server starting wip

---
+++ server.py
@@ -0,0 +1,19 @@
+import discord
+
+SERVERS = {"arma3":"Primary Arma 3"}
+
+class Commands():
+    def register(self, _):
+        return {
+            "start" : {
+                "function" : self.start,
+                "description" : "Start up a service",
+                "roles" : ["manager"]
+            }
+        }
+
+    async def start(self, data, client, message):
+        if data[0].lower() not in SERVERS:
+            await message.channel.send("Unknown Server")
+        else:
+            await message.channel.send("Starting: "+SERVERS[data[0].lower()])
