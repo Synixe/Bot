e24dfcfa20023710f5bcb1aa9f0a29c44376081c
Brett Mayson <brett@bmandesigns.com>
2018-01-17 02:38:18 -0600

    Empty Rewrite

--- tokens.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def getToken(service):
-    with open("./data/tokens.json") as f:
-        data = json.loads(f.read())
-    if service in data:
-        return data[service]
-    else:
-        return None
