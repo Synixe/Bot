6202fd39d09f08c242243626141e6d6a907fdf63
Brett Mayson <brett@bmandesigns.com>
2018-02-10 23:12:48 -0600

    Removed unused param

--- extensions/mods/github.py
+++ extensions/mods/github.py
@@ -1,4 +1,4 @@
-def latest(owner, repo):
+def latest(repo):
     f = urllib.request.urlopen("https://api.github.com/repos/"+repo+"/releases")
     data = json.loads(f.read().decode("UTF-8"))
     return data[0]["tag_name"]
