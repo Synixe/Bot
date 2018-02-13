77ae13dcfda64578b3529e768f9ef347c4cae226
namelessOwO <lulopisntop@gmail.com>
2018-02-13 19:03:44 +1300

    Fixed #24 what a waste of time

--- extensions/monitor/__init__.py
+++ extensions/monitor/__init__.py
@@ -4,17 +4,15 @@ import logger
 class BotExtension:
     def __init__(self, bot):
         self.name = "Server Monitor"
-        self.author = "Brett"
-        self.version = "1.0"
+        self.author = "Brett + nameless"
+        self.version = "1.1"
         self.bot = bot
+        self.disable_during_test = True
 
     async def post_to_bot_events(self, member, text):
         channel = discord.utils.find(lambda c: c.name == "botevents", member.guild.channels)
         if channel is not None:
             await channel.send(text)
-        else:
-            frame = getframeinfo(currentframe())
-            logger.throw("Unable to find #botevents\n\t{0.filename} line {0.lineno - 4}".format(frame))
 
     async def on_member_join(self, member):
         #await self.post_to_bot_events(member, "New Member: {0.display_name}#{0.discriminator} <@{0.id}> ({0.id})".format(member))
@@ -36,7 +34,15 @@ class BotExtension:
 
     async def on_member_update(self, before, after):
         if before.display_name != after.display_name:
-            await self.post_to_bot_events(after, "<@{0.id}> {0.display_name} is now known as {1.display_name}".format(before, after))
+            channel = discord.utils.find(lambda c: c.name == "botevents", after.guild.channels)
+            if channel != None:
+                embed = discord.Embed(
+                    title = "Nickname Change",
+                    color = discord.Colour.from_rgb(r=255,g=192,b=60)
+                )
+                embed.add_field(name="Previous Name", value=before.display_name)
+                embed.add_field(name="New Name", value=after.display_name)
+                await channel.send(embed=embed)
 
     async def on_member_ban(self, g, member):
         await self.post_to_bot_events(member, "<@{0.id}> ({0.name}#{0.discriminator}) has been banned.".format(member))
