caa16d11f6daae5b86d6e84eb90d74404eeea9c5
namelessOwO <lulopisntop@gmail.com>
2018-02-17 14:47:21 +1300

    An attempt to fix welcome message

--- extensions/monitor/__init__.py
+++ extensions/monitor/__init__.py
@@ -24,7 +24,7 @@ class BotExtension:
     def __init__(self, bot):
         self.name = "Server Monitor"
         self.author = "Brett + nameless"
-        self.version = "1.1"
+        self.version = "1.2"
         self.bot = bot
         self.disable_during_test = True
 
@@ -69,13 +69,13 @@ class BotExtension:
             elif channel.name == "repohelp":
                 repohelp = channel
         await lobby.send(
-            ("<@{}>! Welcome to Synixe!"+
+            ("@{}! Welcome to Synixe!"+
             "Here is some basic info about our group: "+
-            "If you check out <#{}> you can see our upcoming missions. "+
+            "If you check out #{} you can see our upcoming missions. "+
             "We play at 7pm PST / 10pm EST. "+
-            "We have some mods you'll need to download from <#{}>. "+
+            "We have some mods you'll need to download from #{}. "+
             "If you have any questions while getting those setup we're more than "+
-            "happy to help in <#{}>.").format(member, events, info, repohelp)
+            "happy to help in #{}.").format(member, events, info, repohelp)
         )
         await member.add_roles(self.get_role(g, "new"))
 
