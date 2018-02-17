d14c364625f5b93bd6a13f2b68b7b90c99433435
Brett Mayson <brett@bmandesigns.com>
2018-02-16 23:31:08 -0600

    Embed fix :wrench:

--- extensions/slotting/embeds.py
+++ extensions/slotting/embeds.py
@@ -3,7 +3,7 @@ import discord
 
 async def display_event(extension, target, event, message):
     """Display an event sheet in #events"""
-    connection = extension.getConnection()
+    connection = extension.get_connection()
     try:
         with connection.cursor() as cursor:
             sql = "SELECT * FROM `events` WHERE `id` = '" + str(event) + "'"
