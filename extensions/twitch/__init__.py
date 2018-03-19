f944f8509681bddf43c23cfd24f560ca70076daf
namelessOwO <lulopisntop@gmail.com>
2018-03-19 20:06:31 +1300

    MISSION FUCKING ACCOMPLISHED #29

--- extensions/twitch/__init__.py
+++ extensions/twitch/__init__.py
@@ -14,24 +14,21 @@ class BotExtension:
 
     async def on_member_update(self, before, after):
         """Streaming notification"""
-        try:
-            if self.bot.in_role_list(after, ["active"]):
-                if after.activity != None and isinstance(after.activity, discord.Streaming):
-                    print(after.activity.details)
-                    if before.activity == None or not isinstance(before.activity, discord.Streaming):
-                        channel = discord.utils.find(lambda c: c.name == "bot", after.guild.channels)
-                        if channel != None:
-                            lines = open('./extensions/twitch/taglines.txt').read().splitlines()
-                            embed = discord.Embed(
-                                title=after.activity.name,
-                                url=after.activity.url,
-                                description=after.activity.details,
-                                color=discord.Colour.from_rgb(r=255, g=192, b=60)
-                            )
-                            embed.set_author(name=after.display_name)
-                            embed.set_thumbnail(url=after.avatar_url)
-                            await channel.send(content=random.choice(lines).format(after.display_name, embed=embed))
-                        else:
-                            logger.error("Unable to find #bot")
-        except Exception as e:
-            logger.error(str(e))
+        if self.bot.in_role_list(after, ["active"]):
+            if after.activity != None and isinstance(after.activity, discord.Streaming):
+                print(after.activity.details)
+                if before.activity == None or not isinstance(before.activity, discord.Streaming):
+                    channel = discord.utils.find(lambda c: c.name == "streams", after.guild.channels)
+                    if channel != None:
+                        lines = open('./extensions/twitch/taglines.txt').read().splitlines()
+                        embed = discord.Embed(
+                            title=after.activity.name,
+                            url=after.activity.url,
+                            description=after.activity.details,
+                            color=discord.Colour.from_rgb(r=255, g=192, b=60)
+                        )
+                        embed.set_author(name=after.display_name)
+                        embed.set_thumbnail(url=after.avatar_url)
+                        await channel.send(embed=embed, content=random.choice(lines).format(after.display_name))
+                    else:
+                        logger.error("Unable to find #bot")
