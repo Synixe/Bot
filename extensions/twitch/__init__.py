bf8251183dd737c0ff43e9d1a54b848ab7713899
Brett Mayson <brett@bmandesigns.com>
2018-03-24 00:56:42 -0600

    delete streams

--- extensions/twitch/__init__.py
+++ extensions/twitch/__init__.py
@@ -15,10 +15,10 @@ class BotExtension:
     async def on_member_update(self, before, after):
         """Posts to #streams if a user starts streaming"""
         if self.bot.in_role_list(after, ["active"]):
-            if after.activity != None and isinstance(after.activity, discord.Streaming):
-                if before.activity == None or not isinstance(before.activity, discord.Streaming):
-                    channel = discord.utils.find(lambda c: c.name == "streams", after.guild.channels)
-                    if channel != None:
+            channel = discord.utils.find(lambda c: c.name == "streams", after.guild.channels)
+            if channel != None:
+                if after.activity != None and isinstance(after.activity, discord.Streaming):
+                    if before.activity == None or not isinstance(before.activity, discord.Streaming):
                         lines = open('./extensions/twitch/taglines.txt').read().splitlines()
                         embed = discord.Embed(
                             title=after.activity.name,
@@ -28,6 +28,18 @@ class BotExtension:
                         )
                         embed.set_author(name=after.display_name)
                         embed.set_thumbnail(url=after.avatar_url)
+                        embed.set_footer(text=after.display_name)
                         await channel.send(embed=embed, content=random.choice(lines).format(after.display_name))
-                    else:
-                        logger.error("Unable to find #bot")
+                elif after.activity == None:
+                    if before.activity != None and isinstance(before.activity, discord.Streaming):
+                        messages = channel.history(limit=50)
+                        target = None
+                        async for msg in messages:
+                            if len(msg.embeds) == 1:
+                                try:
+                                    if msg.embeds[0].footer.text == before.display_name:
+                                        target = msg
+                                except:
+                                    pass
+                        if target != None:
+                            await msg.delete()
