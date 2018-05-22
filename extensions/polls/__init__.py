bad973da3e3c2c5bc8bcd02744a66e8f2eb8f4ac
namelessOwO <lulopisntop@gmail.com>
2018-05-22 22:00:07 +1200

    moved delete

--- extensions/polls/__init__.py
+++ extensions/polls/__init__.py
@@ -11,16 +11,13 @@ class Polls(bot.Extension):
     @bot.command()
     async def poll(ctx, message):
         """Birth of Polls"""
-        if ctx.args.title == None:
-            msg = await message.channel.send(ctx.args.text)
-        else:
-            embed = discord.Embed(
-                title=ctx.args.title,
-                color=discord.Colour.from_rgb(r=255, g=192, b=60),
-                description=ctx.args.text
-            )
-            embed.set_footer(text="Created by: {}".format(message.author.display_name))
-            msg = await message.channel.send(embed=embed)
-        await message.delete()
+        embed = discord.Embed(
+            title=ctx.args.title or "Poll",
+            color=discord.Colour.from_rgb(r=255, g=192, b=60),
+            description=ctx.args.text
+        )
+        embed.set_footer(text="Created by: {}".format(message.author.display_name))
+        msg = await message.channel.send(embed=embed)
         await msg.add_reaction("👍")
         await msg.add_reaction("👎")
+        await message.delete()
