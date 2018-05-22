9393032a22f68ff69fa88f395f52910887b74a45
namelessOwO <lulopisntop@gmail.com>
2018-05-22 19:20:52 +1200

    creation of polls

---
+++ extensions/polls/__init__.py
@@ -0,0 +1,26 @@
+"""Creating Polls for Disco"""
+import discord
+import bot
+
+class Polls(bot.Extension):
+    """Polls for Disco"""
+
+    @bot.role("active")
+    @bot.role("new")
+    @bot.role("inactive")
+    @bot.argument("[title]+")
+    @bot.argument("text+")
+    @bot.command()
+    async def poll(ctx, message):
+        """Birth of Polls"""
+        if ctx.args.title == None:
+            msg = await message.channel.send(" ".join(ctx.args.text))
+        else:
+            embed = discord.Embed(
+                title=ctx.args.title,
+                color=discord.Colour.from_rgb(r=255, g=192, b=60),
+                description=" ".join(ctx.args.text)
+            )
+            msg = await message.channel.send(embed=embed)
+        await msg.add_reaction("👍")
+        await msg.add_reaction("👎")
