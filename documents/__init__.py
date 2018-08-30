dd85c836fdf03a76402cf41ce0157fdcbc5fc7c7
Brett Mayson <brett@bmandesigns.com>
2018-08-30 09:33:35 -0600

    documents

---
+++ documents/__init__.py
@@ -0,0 +1,98 @@
+import discord
+import urllib.request
+
+import bot
+from . import parser
+
+class Documents(bot.Extension):
+    @bot.argument("rule")
+    @bot.command()
+    async def rule(ctx, message):
+        """Display a rule"""
+        try:
+            embed = get_from_latex(
+                "https://raw.githubusercontent.com/Synixe/Documents/master/SynixeRules.tex",
+                ctx.args.rule
+            )
+        except IndexError:
+            await message.channel.send("That rule does not exist.")
+            return
+        if embed != None:
+            if isinstance(embed, discord.Embed):
+                embed.set_author(
+                    name="Synixe Rules",
+                    url="http://synixe.com/rules.html",
+                    icon_url=ctx.bot.user.avatar_url
+                )
+                try:
+                    await message.channel.send(embed=embed)
+                except discord.errors.HTTPException:
+                    await message.channel.send("That section is too large, you'll need to be more specific")
+            else:
+                await message.channel.send(embed)
+
+    @bot.argument("section")
+    @bot.command()
+    async def const(ctx, message):
+        try:
+            embed = get_from_latex(
+                "https://raw.githubusercontent.com/Synixe/Documents/master/SynixeConstitution.tex",
+                ctx.args.section
+            )
+        except IndexError:
+            await message.channel.send("That section does not exist.")
+            return
+        if embed != None:
+            if isinstance(embed, discord.Embed):
+                embed.set_author(
+                    name="Synixe Constitution",
+                    url="http://synixe.com/const.html",
+                    icon_url=ctx.bot.user.avatar_url
+                )
+                try:
+                    await message.channel.send(embed=embed)
+                except discord.errors.HTTPException:
+                    await message.channel.send("That section is too large, you'll need to be more specific")
+            else:
+                await message.chanel.send(embed)
+
+def get_from_latex(url, section):
+    """Get a discord embed from latex"""
+    req = urllib.request.Request(url=url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+    with urllib.request.urlopen(req) as response:
+        tex = parser.Parser(response.read().decode("UTF-8"))
+    rule = tex.get_from_id(section)
+    if isinstance(rule, str):
+        return rule
+    elif isinstance(rule, dict):
+        desc = ""
+        if "text" in rule:
+            desc = tex.process_ref(rule["text"]) + "\n"
+        if "subsections" in rule:
+            x = 0
+            for sub in rule["subsections"]:
+                x += 1
+                desc += "\n__{}.{} - {}__\n{}\n".format(section.split(".")[0], x, sub["name"], tex.process_ref(sub["text"]))
+                i = 0
+                for item in sub['items']:
+                    i += 1
+                    desc += f"{i}. {tex.process_ref(item)}\n"
+                y = 0
+                for ss in sub["subsubsections"]:
+                    y += 1
+                    desc += "\n*{}.{}.{} - {}*\n{}\n".format(section.split(".")[0], x, y, ss["name"], tex.process_ref(ss["text"]))
+        elif "subsubsections" in rule:
+            y = 0
+            for ss in rule["subsubsections"]:
+                y += 1
+                desc += "\n*{}.{}.{} - {}*\n{}\n".format(section.split(".")[0], section.split(".")[1], y, ss["name"], tex.process_ref(ss["text"]))
+        i = 0
+        for item in rule['items']:
+            i += 1
+            desc += f"{i}. {tex.process_ref(item)}\n"
+        embed = discord.Embed(
+            title=section + " " + rule["name"],
+            description=desc,
+            color=discord.Colour.from_rgb(r=255, g=192, b=60)
+        )
+        return embed
---
+++ documents/parser.py
@@ -0,0 +1,91 @@
+"""Parse tex files"""
+import re
+
+class Parser:
+    """TeX Parser"""
+    def __init__(self, data):
+        self.data = data
+        self.document = False
+        self.list = False
+        self.section = -1
+        self.subsection = -1
+        self.subsubsection = -1
+        self.item = -1
+        self.sections = []
+        self.references = {}
+        for line in self.data.split("\n"):
+            line = line.replace("\t", "").replace("\\&", "&")
+            if not self.document:
+                if line == "\\begin{document}":
+                    self.document = True
+            elif not self.list:
+                if line.startswith("\\section"):
+                    self.section += 1
+                    self.subsection = -1
+                    self.sections.append({
+                        "name": line.split("{", 1)[1].split("}")[0],
+                        "subsections": [], "text": "", "items": []
+                    })
+                elif line.startswith("\\subsection"):
+                    self.subsection += 1
+                    self.subsubsection = -1
+                    self.sections[self.section]["subsections"].append({
+                        "name": line.split("{", 1)[1].split("}")[0],
+                        "subsubsections": [], "text": "", "items": []
+                    })
+                elif line.startswith("\\subsubsection"):
+                    self.subsubsection += 1
+                    self.sections[self.section]["subsections"][self.subsection]["subsubsections"].append({
+                        "name": line.split("{", 1)[1].split("}")[0], "text": "", "items": []
+                    })
+                elif line.startswith("\\begin{enumerate}"):
+                    self.list = True
+                    self.item = -1
+                elif not line.startswith("\\") and self.section != -1:
+                    if self.subsection == -1:
+                        self.sections[self.section]["text"] += line + "\n"
+                    elif self.subsubsection == -1:
+                        self.sections[self.section]["subsections"][self.subsection]["text"] += line + "\n\n"
+                    else:
+                        self.sections[self.section]["subsections"][self.subsection]["subsubsections"][self.subsubsection]["text"] += line + "\n\n"
+                if "\\label" in line:
+                    ref = line.split("\\label{")[1].split("}")[0]
+                    if self.subsubsection != -1:
+                        self.references[ref] = "{}.{}.{}".format(self.section + 1, self.subsection + 1, self.subsubsection + 1)
+                    elif self.subsection != -1:
+                        self.references[ref] = "{}.{}".format(self.section + 1, self.subsection + 1)
+                    else:
+                        self.references[ref] = "{}".format(self.section + 1)
+            elif self.list:
+                if line.startswith("\\item"):
+                    self.item += 1
+                    if self.subsection == -1:
+                        self.sections[self.section]["items"].append(line.replace("\\item ", ""))
+                    elif self.subsubsection == -1:
+                        self.sections[self.section]["subsections"][self.subsection]["items"].append(line.replace("\\item ", ""))
+                    else:
+                        self.sections[self.section]["subsections"][self.subsection]["subsubsections"][self.subsubsection]["items"].append(line.replace("\\item ", ""))
+                elif line.startswith("\\end"):
+                    self.list = False
+
+    def process_ref(self, text):
+        """Turn references in section numbers"""
+        return re.sub(r'\\ref{(.+?)}', lambda match: self.references[match.group(1)], text)
+
+    def get_from_id(self, tag):
+        """Get a section by it's tag"""
+        sections = tag.split(".")
+        if len(sections) == 1:
+            return self.sections[int(tag) - 1]
+        elif len(sections) == 2:
+            try:
+                return self.sections[int(sections[0]) - 1]["subsections"][int(sections[1]) - 1]
+            except IndexError:
+                return self.sections[int(sections[0]) - 1]["items"][int(sections[1]) - 1]
+        elif len(sections) == 3:
+            try:
+                return self.sections[int(sections[0]) - 1]["subsections"][int(sections[1]) - 1]["items"][int(sections[2]) - 1]
+            except IndexError:
+                return self.sections[int(sections[0]) - 1]["subsections"][int(sections[1]) - 1]["subsubsections"][int(sections[2]) - 1]
+        elif len(sections) == 4:
+            return self.sections[int(sections[0]) - 1]["subsections"][int(sections[1]) - 1]["subsubsections"][int(sections[2]) - 1]["items"][int(sections[3]) - 1]
