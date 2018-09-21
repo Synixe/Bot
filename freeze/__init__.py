5187fb03cdae3ba7e35c09d5791e43b6d834fc13
Brett Mayson <brett@bmandesigns.com>
2018-09-21 15:28:06 -0600

    freeze command

---
+++ freeze/__init__.py
@@ -0,0 +1,17 @@
+import discord
+import bot
+
+class Freeze(bot.Extension):
+    @bot.argument("member+", discord.Member)
+    @bot.command()
+    async def freeze(ctx, message):
+        """Freeze a user to prevent them from posting"""
+        await ctx.args.member.add_roles(discord.utils.find(lambda m: m.name.lower() == "silenced", ctx.args.member.guild.roles))
+        await message.add_reaction("✅")
+
+    @bot.argument("member+", discord.Member)
+    @bot.command()
+    async def unfreeze(ctx, message):
+        """Unfreeze a user and allow them to post again"""
+        await ctx.args.member.remove_roles(discord.utils.find(lambda m: m.name.lower() == "silenced", ctx.args.member.guild.roles))
+        await message.add_reaction("✅")
