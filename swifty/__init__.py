00fed30fdd920fa5a0f46aa6b7af680f54356268
Brett <brett@bmandesigns.com>
2019-05-26 20:37:20 -0600

    another bug

--- swifty/__init__.py
+++ swifty/__init__.py
@@ -8,6 +8,8 @@ class Swifty(bot.Extension):
         """Swifty Error Responder"""
         if "System.Net.WebException: The request was aborted: Could not create SSL/TLS secure channel.".lower() in message.content.lower():
             await message.channel.send("Due to Swifty refusing to acknowledge that bug exists, HTTPS has been disabled. Please change your repo address to `http://repo.synixe.com`")
+        elif "A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond 69.11.99.171:443".lower() in message.content.lower():
+            await message.channel.send("Due to Swifty refusing to acknowledge that bug exists, HTTPS has been disabled. Please change your repo address to `http://repo.synixe.com`")
         elif "System.IO.IOException: There is not enough space on the disk.".lower() in message.content.lower():
             await message.channel.send("The disk with your mod folder is full, move the mod folder to another disk immediately and change the path. http://repo.synixe.com/swifty3.png")
         elif "System.Exception: Could not locate 'arma3launcher.exe'".lower() in message.content.lower():
