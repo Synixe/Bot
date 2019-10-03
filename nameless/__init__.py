f094e460a8628a611df126bb28c07ae801e8ad03
AndrewJMD <andrewjmd22@gmail.com>
2019-10-03 16:51:58 -0600

    no more delete message
    message is no longer deleted

--- nameless/__init__.py
+++ nameless/__init__.py
@@ -7,4 +7,3 @@ class NamelessText(bot.Extension):
         """Deletes any messages containing nameless and replaces it with Nameless"""
         if "nameless" in message.content:
             await message.channel.send("{}\nThere, fixed that for ya".format(message.content.replace("nameless", "Nameless")))
-            await message.delete()
