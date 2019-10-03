8392e6172020b350fab56421a44915d843072acb
AndrewJMD <andrewjmd22@gmail.com>
2019-10-03 16:48:36 -0600

    deleted .replace()
    no more .replace() and no space between the \n and T

--- nameless/__init__.py
+++ nameless/__init__.py
@@ -5,6 +5,6 @@ class NamelessText(bot.Extension):
     @bot.event("on_message")
     async def replace_nameless(ctx, message):
         """Deletes any messages containing nameless and replaces it with Nameless"""
-        if "nameless" in message.content.replace(" ", ""):
-            await message.channel.send("{}\n There, fixed that for ya".format(message.content.replace("nameless", "Nameless")))
+        if "nameless" in message.content:
+            await message.channel.send("{}\nThere, fixed that for ya".format(message.content.replace("nameless", "Nameless")))
             await message.delete()
